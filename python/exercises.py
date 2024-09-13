1a73b9cd8e448fa278ad7b913c03c53f1a1010cc
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-09-13 08:57:59 -0700

    Removed unnecessary comments, readjusted line spacing, and added file.close()

--- python/exercises.py
+++ python/exercises.py
@@ -3,6 +3,7 @@ from collections.abc import Callable
 from typing import Callable, Optional
 import os
 
+
 def change(amount: int) -> dict[int, int]:
     if not isinstance(amount, int):
         raise TypeError('Amount must be an integer')
@@ -21,6 +22,7 @@ def first_then_lower_case(a: list[str], p: Callable[[str], bool], /) -> Optional
             return string.lower()
     return None
 
+
 # Write your powers generator here
 def powers_generator(*, base, limit):
     exponent = 0
@@ -31,39 +33,44 @@ def powers_generator(*, base, limit):
         yield limit_test
         exponent += 1
 
+
 # Write your say function here
 def say(word=None):
     created_string = []
 
     def next_word(new_word=None):
-        if new_word == "": # empty string 
-            created_string.append(("")) # add space "" to end of list
-            return next_word # chain
-        
-        if new_word: # non empty string
-            created_string.append(new_word) # add new_word to end of created string
+        if new_word == "":  # if empty string, add space to end of list
+            created_string.append((""))
             return next_word  # chain
-        
-        else: # not given anything --> say()
-            return " ".join(created_string)  # Return the final string
-    
+
+        if new_word:  # if non empty string, add new_word to end of created string
+            created_string.append(new_word)
+            return next_word  # chain
+
+        else:  # not given anything --> say()
+            return " ".join(created_string)
+
     return next_word(word)
+
 
 # Write your line count function here
 def meaningful_line_count(file_path):
     if not os.path.exists(file_path):
         raise FileNotFoundError("No such file")
-    
+
     valid_lines = 0
     with open(file_path, "r", encoding="utf-8") as opened_file:
-    # without encoding -> UnicodeDecodeError: 'charmap' codec can't decode byte 0x81 in position 16: character maps to <undefined>
-    # solution found at: https://stackoverflow.com/questions/9233027/unicodedecodeerror-charmap-codec-cant-decode-byte-x-in-position-y-character
+        # without encoding -> UnicodeDecodeError: 'charmap' codec can't decode byte 0x81 in position 16: character maps to <undefined>
+        # solution found at: https://stackoverflow.com/questions/9233027/unicodedecodeerror-charmap-codec-cant-decode-byte-x-in-position-y-character
         for line in opened_file:
-            stripped_line = line.strip() # remove leading and trailing whitespace
-            if stripped_line and not stripped_line.startswith("#"): # check if not empty and not starting with "#"
+            stripped_line = line.strip()
+            # check if not empty and not starting with "#"
+            if stripped_line and not stripped_line.startswith("#"):
                 valid_lines += 1
+        opened_file.close()
 
     return valid_lines
+
 
 # Write your Quaternion class here
 class Quaternion:
