9950e030b3198847daab6105d8ff8f70ee333d5f
Jjasim1 <144839718+Jjasim1@users.noreply.github.com>
2024-09-25 10:49:00 -0700

    Added FirstThenLowerCase and say function

--- swift/exercises.swift
+++ swift/exercises.swift
@@ -16,8 +16,37 @@ func change(_ amount: Int) -> Result<[Int:Int], NegativeAmountError> {
 }
 
 // Write your first then lower case function here
+func firstThenLowerCase<T>(of array: [T], satisfying predicate: (T) -> Bool) -> String? where T: StringProtocol {
+    for element in array {
+        if predicate(element) {
+            return element.lowercased()
+        }
+    }
+    return nil
+}
 
 // Write your say function here
+class say {
+    private var words: [String] = []
+
+    // Initializer for starting with an optional initial word
+    init(_ word: String? = nil) {
+        self.words = word != nil ? [word!] : []
+    }
+
+    // Method to add a word to the chain
+    @discardableResult
+    func and(_ word: String) -> say {
+        let newSay = say()
+        newSay.words = self.words + [word]
+        return newSay
+    }
+
+    // Read-only property to get the accumulated phrase
+    var phrase: String {
+        return words.joined(separator: " ")
+    }
+}
 
 // Write your meaningfulLineCount function here
 
