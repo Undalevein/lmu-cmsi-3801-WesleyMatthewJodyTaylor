02f70261a56c021554ab0f4d14901391e655dcf2
Jjasim1 <144839718+Jjasim1@users.noreply.github.com>
2024-09-30 19:53:01 -0700

    Added BST

--- swift/exercises.swift
+++ swift/exercises.swift
@@ -53,3 +53,57 @@ class say {
 // Write your Quaternion struct here
 
 // Write your Binary Search Tree enum here
+enum BinarySearchTree: CustomStringConvertible{
+    case empty
+    indirect case node(BinarySearchTree, String, BinarySearchTree)
+
+    var size: Int {
+        switch self {
+        case .empty:
+            return 0
+        case .node(let left, _, let right):
+            return 1 + left.size + right.size
+        }
+    }
+
+    func contains(_ value: String) -> Bool {
+        switch self {
+        case .empty:
+            return false
+        case let .node(left, v, right):
+            if value < v {
+                return left.contains(value)
+            } else if value > v {
+                return right.contains(value)
+            } else {
+                return true
+            }
+        }
+    }
+
+    func insert(_ value: String) -> BinarySearchTree {
+        switch self {
+        case .empty:
+            return .node(.empty, value, .empty)
+        case let .node(left, v, right):
+            if value < v {
+                return .node(left.insert(value), v, right)
+            } else if value > v {
+                return .node(left, v, right.insert(value))
+            } else {
+                return self
+            }
+        }
+    }
+
+    var description: String {
+        switch self {
+        case .empty:
+            return "()"
+        case let .node(left, value, right):
+            let leftDesc = left.description == "()" ? "":left.description
+            let rightDesc = right.description == "()" ? "":right.description
+            return "(\(leftDesc)\(value)\(rightDesc))"
+        }
+    }
+}
