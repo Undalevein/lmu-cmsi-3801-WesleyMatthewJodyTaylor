06017ea2639c1acb6d75443917ab24fa26ecd277
Wesley Ng <73354635+Undalevein@users.noreply.github.com>
2024-10-02 02:03:10 -0700

    Added a few more things

--- kotlin/Exercises.kt
+++ kotlin/Exercises.kt
@@ -16,46 +16,45 @@ fun change(amount: Long): Map<Int, Long> {
 
 // Write your first then lower case function here
 fun firstThenLowerCase(stringList: List<String>, predicate: (it: String) -> Boolean): String? {
-    val filteredMap = stringList?.filter{predicate}[0]
-    return filteredMap?.lowercase()
+    return stringList.filter(predicate).getOrNull(0)?.lowercase()
 }
 
 // Write your say function here
-//object PhraseConstructor {
-//    private var sentence = ""
-//
-//    fun and(excerpt: String) {
-//        sentence = sentence + " " + excerpt
-//    }   
-//
-//    val phrase: String
-//        get() = sentence
-//}
+data class PhraseConstructor(val phrase: String){
+    fun and(excerpt: String): PhraseConstructor {
+        return PhraseConstructor(phrase + " " + excerpt)
+    }   
+}
 
-//fun say(firstWord: String = ""): PhraseConstructor{
-//    return PhraseConstructor
-//}
-
-
+fun say(firstWord: String = ""): PhraseConstructor{
+    return PhraseConstructor(firstWord)
+}
 
 // Write your meaningfulLineCount function here
-//fun meaningfulLineCount(fileName: String): Long {
-//    var counter: Long = 0L
-//    File(fileName).use {
-//        input ->
-//        var lines = input.bufferedReader.readLines()
-//        for (line in lines) {
-//            val stripped: String = line.replace("\\s".toRegex(),"")
-//            if (stripped.length > 0 && stripped[0] != '#') {
-//                ++counter
-//            }
-//        }
-//    }
-//    return counter
-//}
+@throwIOError
+fun meaningfulLineCount(filePath: String): Long {
+    var counter: Long = 0L
+    
+    BufferedReader(FileReader(filePath)).use {
+        input ->
+        var line: String? 
+        while (input.readLine().also {line = it} != null) {
+            val stripped: String = line!!.replace("\\s".toRegex(),"")
+            if (stripped.length > 0 && stripped[0] != '#') {
+                ++counter
+            }
+        }
+    }
+    return counter
+}
 
 // Write your Quaternion data class here
-data class Quaternion(val a: Double, val b: Double, val c: Double, val d: Double){
+data class Quaternion(
+    val a: Double, 
+    val b: Double, 
+    val c: Double, 
+    val d: Double
+){
     //companion object {
     //    const val ZERO = Quaternion(0.0, 0.0, 0.0, 0.0)
     //    const val I = Quaternion(0.0, 1.0, 0.0, 0.0)
@@ -71,3 +70,15 @@ data class Quaternion(val a: Double, val b: Double, val c: Double, val d: Double
 }
 
 // Write your Binary Search Tree interface and implementing classes here
+sealed interface BinarySearchTree {
+
+    fun size(): Int 
+    fun contains(item: String): Boolean
+    fun insert(item: String): None
+    override fun toString(): String
+
+}
+
+data class Empty(
+    override val 
+) : BinarySearchTree
